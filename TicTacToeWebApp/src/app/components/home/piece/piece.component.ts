df862ba929f5c009f965cd3dedfda53c14d918a7
FGerber <francois.gerber@angloamerican.com>
2022-01-30 23:41:17 +0200

    Created and designed the main UI of the game

---
+++ TicTacToeWebApp/src/app/components/home/piece/piece.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-piece',
+  templateUrl: './piece.component.html',
+  styleUrls: ['./piece.component.css']
+})
+export class PieceComponent implements OnInit {
+
+  @Input() pieceName: number;
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
